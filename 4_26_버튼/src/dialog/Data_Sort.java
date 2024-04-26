5d775f0d9b90a0c479124ab305b1212eda3b78a1
min0936 <101571808+misnawk@users.noreply.github.com>
2024-04-26 17:20:10 +0900

    Add files via upload

---
+++ 4_26_버튼/src/dialog/Data_Sort.java
@@ -0,0 +1,68 @@
+//package dialog;
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.Iterator;
+//
+//import javax.swing.JLabel;
+//
+//public class Data_Sort {
+////    String jLabels[];
+//    String label;
+//    
+//
+//  
+////	private String[] food;
+////	private String[] count;
+////	private String[] price;
+//
+////    public ArrayList<String[]> getdata() {
+////        return dataList;
+////    }
+//    
+//    public Data_Sort(String message) {
+//    	  String[] data = message.split("@"); // '@' 기준으로 문자열을 분할하여 배열에 저장
+//    	//  label = new String[data.length]; // JLabel 배열을 데이터 길이만큼 생성
+//
+//          for (int i = 0; i < data.length; i++) {
+//        	  label = new String(data[i]); // 각 데이터를 JLabel로 변환하여 배열에 저장
+//          }
+//          
+//          LabelCreate create = new LabelCreate();
+//          create.getLabels(label);
+//         
+//         
+//          
+//          
+////          for (JLabel label : jLabels) {
+////              System.out.println(label.getText());
+////          }
+//      }
+//    
+//    
+//    
+//
+//      // JLabel 배열 반환 메서드
+//    
+//}
+//
+////    	   for(String d1 : data) {
+////    		   	JLabel jLabel = new JLabel();
+////	        	 System.out.println(d1);
+////	        }
+//    	 
+//
+////    	    for (String d : data) {
+////    	        String[] dataPart = d.split(";"); // ';' 기준으로 문자열을 분할하여 배열에 저장
+////    	        
+////    	        for(String d1 : dataPart) {
+////    	        	 System.out.println(d1);
+////    	        }
+////    	       
+////    	        
+////    	}
+//	
+//
+//
+//
+//
---
+++ 4_26_버튼/src/dialog/LabelCreate.java
@@ -0,0 +1,19 @@
+//package com.example.dialog;
+//
+//import javax.swing.JFrame;
+//import javax.swing.JLabel;
+//import javax.swing.JPanel;
+//
+//public class LabelCreate {
+//    private String message;
+//
+//    public void setLabel(String message) {
+//        this.message = message;
+//    }
+//
+//    public void sendLabel(JPanel panel, JLabel label) {
+//        label.setText(message);
+//        System.out.println(label.getText());
+//        System.out.println("출력완료");
+//    }
+//}
