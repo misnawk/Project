eea9ecbb0020fa460c6188bb69c2fade68a507b8
min0936 <101571808+misnawk@users.noreply.github.com>
2024-04-28 16:37:14 +0900

    Add files via upload

---
+++ day04_28/src/day04_27/DMain.java
@@ -0,0 +1,19 @@
+package day04_27;
+
+import java.awt.EventQueue;
+
+public class DMain {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(() -> {
+			try {
+				// DialogForm의 인스턴스를 생성하고 show() 메서드 호출
+
+				DialogForm dialogForm = DialogForm.getInstance();
+				dialogForm.show();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+	}
+}
